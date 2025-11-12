f1aa72e94f5367fda08a67d380c4e0868e5ecc62
2022 <ivan.parysov@mail.ru>
2025-11-12 05:13:23 +0300

    Добавьте файлы проекта.

---
+++ Doctor/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using SqlExplorer;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Doctor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly List<string> SqlQueries = new List<string>
+        {
+            @"
+            IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'doctor_test')
+            BEGIN
+                CREATE DATABASE doctor_test;
+            END;
+            ",
+            "USE doctor_test;",
+            @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'permissions')
+            BEGIN
+                CREATE TABLE permissions (
+                    id INT PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL
+                );
+            END;
+            ",
+            @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'users')
+            BEGIN
+                CREATE TABLE users (
+                    id INT PRIMARY KEY,
+                    permission_id INT,
+                    name VARCHAR(32),
+                    password VARCHAR(64),
+                    FOREIGN KEY (permission_id) REFERENCES permissions(id)
+                );
+            END;
+            ",
+            @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'disease')
+            BEGIN
+                CREATE TABLE disease (
+                    id INT PRIMARY KEY,
+                    name VARCHAR(96),
+                    procedures VARCHAR(1024)
+                );
+            END;
+            ",
+            @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'symptoms')
+            BEGIN
+                CREATE TABLE symptoms (
+                    id INT PRIMARY KEY,
+                    name VARCHAR(256)
+                );
+            END;
+            ",
+            @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'medicines')
+            BEGIN
+                CREATE TABLE medicines (
+                    id INT PRIMARY KEY,
+                    name VARCHAR(96),
+                    quantity INT
+                );
+            END;
+            ",
+            @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'diseases_symptoms')
+            BEGIN
+                CREATE TABLE diseases_symptoms (
+                    disease_id INT,
+                    symptom_id INT,
+                    PRIMARY KEY (disease_id, symptom_id),
+                    FOREIGN KEY (disease_id) REFERENCES disease(id),
+                    FOREIGN KEY (symptom_id) REFERENCES symptoms(id)
+                );
+            END;
+            ",
+            @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'diseases_medicines')
+            BEGIN
+                CREATE TABLE diseases_medicines (
+                    medicines_id INT,
+                    diseases_id INT,
+                    quantity INT,
+                    PRIMARY KEY (medicines_id, diseases_id),
+                    FOREIGN KEY (medicines_id) REFERENCES medicines(id),
+                    FOREIGN KEY (diseases_id) REFERENCES disease(id)
+                );
+            END;
+            ",
+            @"
+            IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'interchangeable_medicines')
+            BEGIN
+                CREATE TABLE interchangeable_medicines (
+                    medicine_id INT,
+                    interchangeable_id INT,
+                    PRIMARY KEY (medicine_id, interchangeable_id),
+                    FOREIGN KEY (medicine_id) REFERENCES medicines(id),
+                    FOREIGN KEY (interchangeable_id) REFERENCES medicines(id)
+                );
+            END;
+            "
+        };
+
+        public static SqlConnector sqlConnector = new SqlConnector();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (var query in SqlQueries)
+            {
+                sqlConnector.Push(query);
+            }
+        }
+    }
+}
---
+++ Doctor/SqlConnector.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace SqlExplorer
+{
+    /// <author>Автор: https://github.com/Rediska4445</author>
+    /// <summary>
+    /// Представляет обёртку для подключения к SQL Server и выполнения основных операций с базой данных.
+    /// Предоставляет методы открытия/закрытия соединения, выполнения запросов и обработки событий ключевых операций.
+    /// </summary>
+    public class SqlConnector
+    {
+        /// <summary>
+        /// Строка подключения к базе данных.
+        /// По умолчанию использует локальную базу данных SQL Server (localdb).
+        /// </summary>
+        public string connectionString = @"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;";
+
+        /// <summary>
+        /// Объект SqlConnection, управляющий состоянием соединения с базой данных.
+        /// </summary>
+        private SqlConnection connection;
+
+        /// <summary>
+        /// Делегат, вызываемый при успешном открытии соединения.
+        /// </summary>
+        public Action<SqlConnection> onOpen;
+
+        /// <summary>
+        /// Делегат, вызываемый при закрытии соединения.
+        /// </summary>
+        public Action<SqlConnection> onClose;
+
+        /// <summary>
+        /// Делегат, вызываемый после успешной операции вставки или обновления.
+        /// </summary>
+        public Action<SqlConnection> onPush;
+
+        /// <summary>
+        /// Делегат, вызываемый после выполнения операции чтения данных.
+        /// </summary>
+        public Action<SqlConnection> onRead;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса, создавая подключение с использованием строки по умолчанию.
+        /// </summary>
+        public SqlConnector()
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса с предоставленной строкой подключения.
+        /// </summary>
+        /// <param name="conn"> строка подключения к базе данных </param>
+        public SqlConnector(string conn)
+        {
+            connection = new SqlConnection(connectionString = conn);
+        }
+
+        /// <summary>
+        /// Выполняет SQL-запрос и возвращает заполненную DataTable.
+        /// Используется для получения данных.
+        /// </summary>
+        /// <param name="sql">SQL-запрос в виде строки</param>
+        /// <returns>таблица с результатами запроса</returns>
+        public DataTable Fill(string sql)
+        {
+            DataTable table = new DataTable();
+
+            // Открываем соединение, если оно закрыто
+            if (connection.State != ConnectionState.Open)
+                Open();
+
+            if (connection.State == ConnectionState.Open)
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                adapter.Fill(table);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// Открывает соединение с базой данных.
+        /// В случае успеха вызывает событие onOpen.
+        /// </summary>
+        public void Open()
+        {
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+
+            onOpen?.Invoke(connection);
+        }
+
+        /// <summary>
+        /// Закрывает соединение.
+        /// В случае успеха вызывает событие onClose.
+        /// </summary>
+        public void Close()
+        {
+            if (connection.State != ConnectionState.Closed)
+                connection.Close();
+
+            onClose?.Invoke(connection);
+        }
+
+        /// <summary>
+        /// Выполняет оператор SELECT и вызывает переданную лямбду для обработки каждой строки.
+        /// </summary>
+        /// <param name="sql">SQL-запрос для чтения</param>
+        /// <param name="processRow">Лямбда, которая обрабатывает каждую строку</param>
+        public void Read(string sql, Action<SqlDataReader> processRow)
+        {
+            if (connection.State != ConnectionState.Open)
+                Open();
+
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    processRow(reader);
+                }
+            }
+
+            onRead?.Invoke(connection);
+        }
+
+        /// <summary>
+        /// Выполняет оператор INSERT/UPDATE/DELETE.
+        /// Перед выполнением вызывается переданный делегат для настройки команды.
+        /// </summary>
+        /// <param name="sql">SQL-команда</param>
+        /// <param name="configureCommand">Лямбда для настройки SqlCommand (например, добавление параметров)</param>
+        public void Push(string sql, Action<SqlCommand> configureCommand)
+        {
+            if (connection.State != ConnectionState.Open)
+                Open();
+
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                if(configureCommand != null) 
+                    configureCommand?.Invoke(command);
+
+                command.ExecuteNonQuery();
+            }
+
+            onPush?.Invoke(connection);
+        }
+
+        /// <summary>
+        /// Выполняет оператор INSERT/UPDATE/DELETE.
+        /// Перед выполнением вызывается переданный делегат для настройки команды.
+        /// </summary>
+        /// <param name="sql">SQL-команда</param>
+        /// <param name="configureCommand">Лямбда для настройки SqlCommand (например, добавление параметров)</param>
+        public void Push(string sql)
+        {
+            Push(sql, null);
+        }
+
+        /// <summary>
+        /// Возвращает текущий экземпляр SqlConnection, используемый для запросов.
+        /// </summary>
+        /// <returns>SqlConnection</returns>
+        public SqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
